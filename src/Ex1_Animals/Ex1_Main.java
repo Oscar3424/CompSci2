1e30d7cd1fa7dae296b178e821a7b6ee9b61cc19
o.lu <Lu.oscar07@gmail.com>
2024-12-11 15:23:16 -0700

    com

--- src/Ex1_Animals/Ex1_Main.java
+++ src/Ex1_Animals/Ex1_Main.java
@@ -11,11 +11,21 @@ public class Ex1_Main {
         allAnimals.add(new Rotweiller( "Rubble" ));
         allAnimals.add( new Shihtzu("Bubbles"));
         allAnimals.add (new Trout( "Slippery" ));
+        allAnimals.add(new Camel("Sandy",4));
 
+        System.out.println("What does the dogs say?");
         for (int i = 0; i < allAnimals.size(); i++) {
             if(allAnimals.get(i) instanceof Dog) {
                 allAnimals.get(i).talk();
             }
         }
+
+        allAnimals.get(5).talk();
+
+        Animal a = new Rotweiller("Mean old doggie");
+//        Rotweiller b = new Animal("Jason was wrong");
+        a.talk();
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import Ex1_Animals.Ex1_Main;
+import Ex2_Restaurant.Ex2_Main;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Inheritance Project");
-        Ex1_Main.run();
+//        Ex1_Main.run(); //Animals
+        Ex2_Main.run(); //restaurants
     }
 
 
