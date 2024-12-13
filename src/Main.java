4817e1538d6eeeca67fea7b2de9d0ee1a46f1233
o.lu <Lu.oscar07@gmail.com>
2024-12-13 15:23:32 -0700

    com

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import Ex1_Animals.Ex1_Main;
 import Ex2_Restaurant.Ex2_Main;
+import Ex3_MusicFestival.Ex3_Main;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Inheritance Project");
 //        Ex1_Main.run(); //Animals
-        Ex2_Main.run(); //restaurants
+//        Ex2_Main.run(); //restaurants
+        Ex3_Main.run();
     }
 
 
