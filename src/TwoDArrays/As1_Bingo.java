74618de4d6fd4a5728e72f07aaa300a704f16c31
o.lu <Lu.oscar07@gmail.com>
2025-01-31 14:55:44 -0700

    com

---
+++ src/TwoDArrays/As1_Bingo.java
@@ -0,0 +1,12 @@
+package TwoDArrays;
+
+public class As1_Bingo {
+    public static void run() {
+        char [] bingoArray = {'b','i','n','g','o'};
+
+    }
+
+    void generateArray(l,min,max){
+        int [] intArray = {};
+    }
+}
